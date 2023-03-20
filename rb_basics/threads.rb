ac244ea787d34c9bdfa4bcb066a28fcb29a29d9f
KavitaJadhav <jadhavkavita206@gmail.com>
2023-03-20 21:20:21 +0530

    Updated thread snippets.

--- rb_basics/threads.rb
+++ rb_basics/threads.rb
@@ -1,24 +1,44 @@
+class TestThread
+  def call
+    Thread.new { puts 'in thread' }
+    puts 'hello'
+  end
+end
+
+k = TestThread.new
+k.call # Main Thread doesn't wait for completion of new thread
+# hello
+# => nil
+# in thread
+
+# ---------
 class ThreadDemo1
   def call
     list = [1, 2, 3, 4]
 
     list.each do |num|
-      Thread.new(num) {|element|
+      Thread.new(num) { |element|
         puts "Started #{element}"
       }
     end
   end
 end
 
-# thread = ThreadDemo1.new
-# thread.call
+thread = ThreadDemo1.new
+thread.call # Thread will execute in parallel. (no sequence)
+# => [1, 2, 3, 4]
+# Started 2
+# Started 3
+# Started 4
+# Started 1
 
+# ---------
 class ThreadDemo2
   def call
     list = [1, 2, 3, 4]
     threads = []
     list.each do |num|
-      threads << Thread.new(num) {|element|
+      threads << Thread.new(num) { |element|
         puts "Started #{element}"
       }
     end
@@ -27,61 +47,119 @@ class ThreadDemo2
   end
 end
 
+ThreadDemo2.new.call # wait for other threads to complete before closing main thread
+# Started 2
+# Started 1
+# Started 4
+# Started 3
+# =>
+#   [#<Thread:0x0000000107fe3a00 (irb):275 dead>,
+#     #<Thread:0x0000000107fe3460 (irb):275 dead>,
+#     #<Thread:0x0000000107fe2ec0 (irb):275 dead>,
+#     #<Thread:0x0000000107fe2d80 (irb):275 dead>]
 
-# thread = ThreadDemo2.new
-# thread.call
-
-
-class ThreadDemo3
-  def call
-    test = 5
-
-    thread = Thread.new do
-      puts test
-
-      sleep 1
-      puts test
+class TestThread
+  def call1
+    threads = []
+    [1, 2, 3, 4].each do |num|
+      Thread.start(num) { |num| puts "Started #{num}" }
     end
-    test = 10
+    puts 'After loop'
+    threads.each(&:jon)
+    puts 'After join'
+  end
 
+  def call2
+    puts 'method call2'
+  end
 
+  def call3
+    call1
+    call2
+    puts 'method call3'
+  end
+end
+
+TestThread.new.call3 # Method doesn't wait for completion of thread. Calls next method. Wait for other threads to complete before closing main thread
+# After loop
+# After join
+# method call2
+# method call3
+# => nil
+# Started 4
+# Started 3
+# Started 1
+# Started 2
+
+# -------------
+class TestThread
+  def call
+    num = 10
+    puts "before thread - Num - #{num}"
+
+    thread = Thread.start do
+      puts "Thread - Num #{num}"
+      sleep 1
+      puts "Thread - Num #{num}"
+    end
+    num = 5
+    puts "after thread - Num - #{num}"
     thread.join
   end
 end
-#
-#
-# thread = ThreadDemo3.new
-# thread.call
+
+TestThread.new.call # Thread will use updated value for variable from the moment value is changed
+# before thread - Num - 10
+# after thread - Num - 5
+# Thread - Num 5
+# Thread - Num 5
 
 class ThreadDemo4
   def call
     i = 0
     threads = []
-    5.times do
-      i = i + 1
+    500.times do
 
       threads << Thread.new do
         # sleep 1
         puts i
       end
+      i = i + 1
     end
 
     threads.each(&:join)
   end
 end
 
-# thread = ThreadDemo4.new
-# thread.call
+ThreadDemo4.new.call # Its always printing last value in the loop. Figure out reason
 
+# 500
+# 500
+# 500
+# 500
+# 500
+# 500
 class ThreadDemo4
-  def call
-    require 'thread'
+  def call_with_param
 
     value = 'original'
 
     t = Thread.new(value) do |t_value|
-      sleep 1
       puts "#{t_value}- in thread"
+    end
+
+    value = 'changed'
+    puts "#{value}- outside thread"
+
+    t.join
+  end
+
+  def call_without_param
+
+    value = 'original'
+
+    t = Thread.new do
+      puts "#{value}- in thread"
     end
 
     value = 'changed'
@@ -91,8 +169,9 @@ class ThreadDemo4
   end
 end
 
-
-thread = ThreadDemo4.new
-thread.call
-
-
+ThreadDemo4.new.call_with_param # original value can be used in thread by passing it as parameter
+# changed- outside thread
+# original- in thread
+ThreadDemo4.new.call_without_param
+# changed- outside thread
+# changed- in thread
